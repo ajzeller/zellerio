550338ec2d65dbd34322719ba99f24659486a5c1
Andrew Zeller <andrew.j.zeller@gmail.com>
2018-12-17 07:40:05 -0800

    adds projects and photo pages

---
+++ js/scroll_reveal.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal();
+sr.reveal('.animate1', { scale: 1, duration: 2000, distance:'20px', viewFactor: .2, easing: 'cubic-bezier(.2, 0.5, 0.5, 1)'}); // animate up
+
+sr.reveal('.animate2', { scale: 1, duration: 1000, distance:'0px', viewFactor: .5, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'}); // fade in
+
+sr.reveal('.animate3', { scale: 1, duration: 1000, distance:'0px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+
+sr.reveal('.animate4', { scale: 1, duration: 1000, origin: 'right', distance:'20px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+
+sr.reveal('.animate5', { scale: 1, duration: 2000, distance:'30px', viewFactor: .5, easing: 'cubic-bezier(.2, 0.5, 0.5, 1)'}); // animate up
