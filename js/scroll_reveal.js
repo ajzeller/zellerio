6b46672da01d6edfad00e1c0e5b8447eeaf3ccdc
Andrew Zeller <andrew.j.zeller@gmail.com>
2018-12-18 22:48:41 -0800

    adds project page layout

--- js/scroll_reveal.js
+++ js/scroll_reveal.js
@@ -5,6 +5,6 @@ sr.reveal('.animate2', { scale: 1, duration: 1000, distance:'0px', viewFactor: .
 
 sr.reveal('.animate3', { scale: 1, duration: 1000, distance:'0px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
-sr.reveal('.animate4', { scale: 1, duration: 1000, origin: 'right', distance:'20px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+sr.reveal('.animate-projects-navbar', { scale: 1, duration: 1000, origin: 'bottom', distance:'400px', viewFactor: .1, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
 sr.reveal('.animate5', { scale: 1, duration: 2000, distance:'30px', viewFactor: .5, easing: 'cubic-bezier(.2, 0.5, 0.5, 1)'}); // animate up
