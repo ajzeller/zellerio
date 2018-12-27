8b4f88b98fca933801256606c48ec7b899b79478
Andrew Zeller <andrew.j.zeller@gmail.com>
2018-12-27 17:10:10 -0600

    reduce scroll slide in distance

--- js/scroll_reveal.js
+++ js/scroll_reveal.js
@@ -7,10 +7,10 @@ window.sr = ScrollReveal();
     sr.reveal('.animate2', { scale: 1, duration: 1000, distance:'0px', viewFactor: .5, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'}); // fade in
 
     // slide in from left
-    sr.reveal('.animate3', { scale: 1, duration: 500, origin: 'left', distance:'100px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+    sr.reveal('.animate3', { scale: 1, duration: 500, origin: 'left', distance:'30px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
     // slide in from right
-    sr.reveal('.animate4', { scale: 1, duration: 500, origin: 'right', distance:'100px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+    sr.reveal('.animate4', { scale: 1, duration: 500, origin: 'right', distance:'30px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
 sr.reveal('.animate-projects-navbar', { scale: 1, duration: 1000, origin: 'bottom', distance:'400px', viewFactor: .1, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
