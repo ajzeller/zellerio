290205d251daa258c1bedf8ba92529ec87e550d5
Andrew Zeller <andrew.j.zeller@gmail.com>
2018-12-27 16:46:50 -0600

    added mytop50 details

--- js/scroll_reveal.js
+++ js/scroll_reveal.js
@@ -1,9 +1,16 @@
 window.sr = ScrollReveal();
-sr.reveal('.animate1', { scale: 1, duration: 2000, distance:'20px', viewFactor: .2, easing: 'cubic-bezier(.2, 0.5, 0.5, 1)'}); // animate up
 
-sr.reveal('.animate2', { scale: 1, duration: 1000, distance:'0px', viewFactor: .5, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'}); // fade in
+    // animate up
+    sr.reveal('.animate1', { scale: 1, duration: 2000, distance:'20px', viewFactor: .2, easing: 'cubic-bezier(.2, 0.5, 0.5, 1)'}); // animate up
 
-sr.reveal('.animate3', { scale: 1, duration: 1000, distance:'0px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+    // fade in
+    sr.reveal('.animate2', { scale: 1, duration: 1000, distance:'0px', viewFactor: .5, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'}); // fade in
+
+    // slide in from left
+    sr.reveal('.animate3', { scale: 1, duration: 500, origin: 'left', distance:'100px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
+
+    // slide in from right
+    sr.reveal('.animate4', { scale: 1, duration: 500, origin: 'right', distance:'100px', viewFactor: .3, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
 sr.reveal('.animate-projects-navbar', { scale: 1, duration: 1000, origin: 'bottom', distance:'400px', viewFactor: .1, easing: 'cubic-bezier(1, 0.5, 0.5, .5)'});
 
